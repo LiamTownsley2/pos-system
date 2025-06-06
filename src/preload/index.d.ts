71acdd76d0f9cbb38d387131217700b381902705
Liam Townsley <contact@liamtownsley.me>
2025-06-06 18:18:05 +0100

    v1.0.11

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -3,8 +3,6 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: {
-      getAppVersion: () => Promise<string>
-    }
+    api: unknown
   }
 }
