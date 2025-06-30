7eaffea8f3d8a676b7319c3ccf927fd87d6f0e2c
Liam Townsley <contact@liamtownsley.me>
2025-07-01 00:02:30 +0100

    v1.1.8

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -2,8 +2,12 @@ import Database, { Database as DatabaseType } from 'better-sqlite3'
 import runMigrations from './migrations/createTables'
 import { app } from 'electron'
 import { join } from 'path'
+import { existsSync, mkdirSync } from 'fs'
 
 const globalDataPath = join('C:', 'ProgramData', 'pos-system')
+if (!existsSync(globalDataPath)) {
+  mkdirSync(globalDataPath, { recursive: true })
+}
 app.setPath('userData', globalDataPath)
 const db = new Database(join(app.getPath('userData'), 'app.db'))
 
